35f76749d9fe828e877b048436ca5de07acb0224
irenka <nikoniuknatalia777@gmail.com>
2023-11-20 18:05:13 +0100

    git push origin main

--- script.js
+++
@@ -1,4 +0,0 @@
-/* mouseover and mouseout events to `.wrapper` element */
-var $wrapper = document.querySelector(".top");
-$top.addEventListener("mouseover", displayTxt);
-$top.addEventListener("mouseout", removeTxt);
